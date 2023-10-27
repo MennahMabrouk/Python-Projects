8d1e68a4d811c129b9093dddedb35efba4bb7b5e
MennahTullah Mabrouk <101124995+MennahMabrouk@users.noreply.github.com>
2023-10-27 19:57:33 +0300

    Update 7. Bidirectional Search TicTacToe.py

--- TicTacToe/7. Bidirectional Search TicTacToe.py
+++ TicTacToe/7. Bidirectional Search TicTacToe.py
@@ -2,35 +2,24 @@ import pygame
 import sys
 from collections import deque
 
-# Define colors
 WHITE = (255, 255, 255)
 PURPLE = (128, 0, 128)
 YELLOW = (255, 255, 0)
 
-# Initialize pygame
 pygame.init()
 
-# Set up the game window
 WINDOW_SIZE = (400, 450)
 screen = pygame.display.set_mode(WINDOW_SIZE)
 pygame.display.set_caption("Tic Tac Toe")
-
-# Set up the game board
 BOARD_SIZE = 3
 board = [["" for _ in range(BOARD_SIZE)] for _ in range(BOARD_SIZE)]
 
-# Set up the fonts
 score_font = pygame.font.Font(None, 30)
 reset_font = pygame.font.Font(None, 24)
-
-# Variable to keep track of the current player's turn
 turn = "X"
-
-# Variables to keep track of player scores
 score_X = 0
 score_O = 0
 
-# Function to draw the score panel and reset button
 def draw_score_panel():
     score_panel_rect = pygame.Rect(0, 0, WINDOW_SIZE[0], 40)
     pygame.draw.rect(screen, PURPLE, score_panel_rect)
@@ -43,7 +32,6 @@ def draw_score_panel():
     reset_text = reset_font.render("Reset", True, PURPLE)
     screen.blit(reset_text, (WINDOW_SIZE[0] - 75, 12))
 
-# Function to draw the game board
 def draw_board():
     screen.fill(PURPLE)
     draw_grid()
@@ -55,13 +43,11 @@ def draw_board():
             elif board[row][col] == "O":
                 draw_o(col * WINDOW_SIZE[0] // BOARD_SIZE, row * WINDOW_SIZE[1] // BOARD_SIZE)
 
-# Function to draw the grid lines
 def draw_grid():
     for i in range(1, BOARD_SIZE):
         pygame.draw.line(screen, WHITE, (i * WINDOW_SIZE[0] // BOARD_SIZE, 0), (i * WINDOW_SIZE[0] // BOARD_SIZE, WINDOW_SIZE[1]), 3)
         pygame.draw.line(screen, WHITE, (0, i * WINDOW_SIZE[1] // BOARD_SIZE), (WINDOW_SIZE[0], i * WINDOW_SIZE[1] // BOARD_SIZE), 3)
 
-# Function to draw the "X" symbol
 def draw_star(x, y):
     x_center = x + WINDOW_SIZE[0] // (2 * BOARD_SIZE)
     y_center = y + WINDOW_SIZE[1] // (2 * BOARD_SIZE)
@@ -72,14 +58,12 @@ def draw_star(x, y):
     pygame.draw.line(screen, WHITE, (x_center - radius, y_center - radius), (x_center + radius, y_center + radius), 5)
     pygame.draw.line(screen, WHITE, (x_center - radius, y_center + radius), (x_center + radius, y_center - radius), 5)
 
-# Function to draw the "O" symbol
 def draw_o(x, y):
     radius = WINDOW_SIZE[0] // (2 * BOARD_SIZE) - 5
     center_x = x + WINDOW_SIZE[0] // (2 * BOARD_SIZE)
     center_y = y + WINDOW_SIZE[1] // (2 * BOARD_SIZE)
     pygame.draw.circle(screen, WHITE, (center_x, center_y), radius, 3)
 
-# Function to check for a win
 def check_win(board, player):
     for i in range(BOARD_SIZE):
         if all(board[i][j] == player for j in range(BOARD_SIZE)):
@@ -92,11 +76,9 @@ def check_win(board, player):
         return True
     return False
 
-# Function to check for a tie
 def check_tie(board):
     return all(board[i][j] != "" for i in range(BOARD_SIZE) for j in range(BOARD_SIZE))
 
-# Function to reset the game
 def reset_game():
     global board, turn
     board = [["" for _ in range(BOARD_SIZE)] for _ in range(BOARD_SIZE)]
@@ -105,7 +87,6 @@ def reset_game():
     draw_score_panel()
     pygame.display.flip()
 
-# Define move costs
 COST_X = 1
 COST_O = 2
 
@@ -158,7 +139,7 @@ def computer_move_bidirectional():
 
         if best_move:
             row, col = best_move
-            board[row][col] = "O"  # Make the best move for Player O
+            board[row][col] = "O" 
 
 
 def main():
